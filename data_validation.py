d7b0b99d07cb6f2534eefb7db8c45902cdab6572
errasti13 <jon44errasti@gmail.com>
2023-08-08 12:04:58 +0200

    Standard Deviation Check

--- data_validation.py
+++ data_validation.py
@@ -1,25 +1,16 @@
 import numpy as np
+import math
 
-def st_dev_check(u,v,G,n_x,n_y,n_g):
-    mean_u = u.mean()
-    mean_v = v.mean()
-    mean_g = G.mean()
-    st_dev_u = u.std()
-    st_dev_v = v.std()
-    st_dev_g = G.std()
-    
-    u_min = mean_u - n_x*st_dev_u
-    u_max = mean_u + n_x*st_dev_u
-    v_min = mean_v - n_y*st_dev_v
-    v_max = mean_v + n_y*st_dev_v
-    g_min = mean_g - n_g*st_dev_g
-    g_max = mean_g + n_g*st_dev_g
-    
-    u[u<u_min] = 0
-    u[u>u_max] = 0
-    v[v<v_min] = 0
-    v[v>v_max] = 0
-    G[G<g_min] = 0
-    G[G>g_max] = 0
-    
-    return u,v,G 
+def st_dev_check(u, v, G, n_x, n_y, n_g):
+    mean_u, mean_v, mean_g = np.mean(u), np.mean(v), np.mean(G)
+    st_dev_u, st_dev_v, st_dev_g = np.std(u), np.std(v), np.std(G)
+
+    u_min, u_max = mean_u - n_x * st_dev_u, mean_u + n_x * st_dev_u
+    v_min, v_max = mean_v - n_y * st_dev_v, mean_v + n_y * st_dev_v
+    g_min, g_max = mean_g - n_g * st_dev_g, mean_g + n_g * st_dev_g
+
+    u = np.where((u >= u_min) & (u <= u_max), u, np.nan)
+    v = np.where((v >= v_min) & (v <= v_max), v, np.nan)
+    G = np.where((G >= g_min) & (G <= g_max), G, np.nan)
+
+    return u, v, G
